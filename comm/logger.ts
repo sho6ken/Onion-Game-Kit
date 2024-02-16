0f4318d55e5048989a81ec882cafa62a6288e85d
Kent <>
2024-02-16 15:24:38 +0800

    feat: 打印日誌

--- comm/logger.ts
+++ comm/logger.ts
@@ -37,4 +37,51 @@ export class Logger {
     private static opened(type: LogType): boolean {
         return (this._flags & type) != 0;
     }
+
+    /**
+     * 打印日誌
+     * @param type 種類
+     * @param msg 訊息
+     * @param hint 補充說明
+     */
+    public static print(type: LogType, msg: any, hint: string = ""): void {
+        if (this.opened(type)) {
+            let conf = LogConf[type];
+
+            let color = conf.color;
+            let time = this.getNowStr();
+            let title = conf.title;
+            let stack = this.getCallStack();
+
+            console.log(`%c[%s][%s][%s][%s]:%o`, color, time, title, stack, hint, msg);
+        }
+    }
+
+    /**
+     * 取得當前時間
+     */
+    private static getNowStr(): string {
+        let res = "";
+
+        // 空位補0並加入字串
+        let func = function(time: number, count: number, symbol: string = ""): void {
+            res += (Array(3).join("0") + time).slice(-count) + symbol;
+        }
+
+        let date = new Date();
+        func(date.getHours(), 2, `:`);
+        func(date.getMinutes(), 2, `:`);
+        func(date.getSeconds(), 2, `:`);
+        func(date.getMilliseconds(), 3);
+
+        return res;
+    }
+
+    /**
+     * 取得呼叫堆疊
+     * @param idx 
+     */
+    private static getCallStack(idx: number = 5): string {
+        return "";
+    }
 }
