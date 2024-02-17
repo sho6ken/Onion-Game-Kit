e6bd61a1084b795a8ee15dc692d0b3dd03cc58c7
Kent <>
2024-02-17 22:16:46 +0800

    feat: 單例型別

--- comm/util/singleton.ts
+++ comm/util/singleton.ts
@@ -25,3 +25,20 @@ export interface Singleton {
      */
     info?(): void;
 }
+
+/**
+ * 單例型別
+ * @summary 用來限制當作參數傳遞的物件一定要具備指定功能
+ */
+interface SingleType<T extends Singleton> {
+    // 名稱
+    name: string;
+
+    // 指定在其他地方建立的實體, 用在cmpt的單例
+    inst?: T;
+
+    /**
+     * 建構子
+     */
+    new(): T;
+}
