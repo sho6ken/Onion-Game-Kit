7e599f1a0427bed93780bb543e606fd5f1dd9a76
Kent <>
2024-02-25 11:58:50 +0800

    feat: 事件回調裝飾

--- comm/event/event-decor.ts
+++ comm/event/event-decor.ts
@@ -1,10 +1,11 @@
-import { EventMgr } from "./event-mgr";
+import { EventMgr, EventType } from "./event-mgr";
 
 /**
  * 事件類別裝飾
  * @param on 調用此函式時註冊事件
  * @param off 調用此函式時註銷事件
  * @summary 非cmpt使用時需自定註冊及註銷函式名稱
+ * @summary 沒有註冊註銷的需求可不使用
  */
 export function eventClass(on: string = "onEnable", off: string = "onDisable"): Function {
     return function(self: any): void {
@@ -33,5 +34,23 @@ export function eventClass(on: string = "onEnable", off: string = "onDisable"): 
         else if (!on) {
             console.warn(`event class=${self.name} off=${off} without register`);
         }
+    }
+}
+
+/**
+ * 事件回調裝飾
+ * @param type 事件種類
+ * @param once 單次觸發
+ */
+export function eventFunc(type: EventType, once: boolean = false): Function {
+    return function (self: any, name: string, desc: PropertyDescriptor): void {
+        let list = EventMgr.rec.get(self.constructor);
+
+        if (!list) {
+            list = [];
+            EventMgr.rec.set(self.constructor, list);
+        }
+
+        list.push({ type: type, cb: name, once: once });
     }
 }
